c2697cf7685342456787812cbb6a543179e3a396
NicolePell <pellicena@gmail.com>
2014-09-30 20:20:03 +0100

    Added 5 dishes with price and name

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -12,7 +12,23 @@ class Dishes
 	# end
 
 	def self.chicken_teriyaki_bento
-		new(8.95,"Chicken Teriyaki Bento")
+		new(8.95, 'Chicken Teriyaki Bento')
+	end
+
+	def self.tonkotsu_ramen
+		new(9.95, 'Tonkotsu Ramen')
+	end
+
+	def self.miso_soup
+		new(5.45, 'Miso Soup')
+	end
+
+	def self.sushi_selection
+		new(12.00, 'Sushi Selection')
+	end
+
+	def self.wagyu_beef
+		new(15.00, 'Wagyu Beef')
 	end
 
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -4,6 +4,10 @@ describe Dishes do
 
 	let(:dishes) { Dishes.new(0) }
 	let(:ctb) { Dishes.chicken_teriyaki_bento }
+	let(:tr) { Dishes.tonkotsu_ramen }
+	let(:ms) { Dishes.miso_soup }
+	let(:ss) { Dishes.sushi_selection }
+	let(:wb) { Dishes.wagyu_beef }
 	
 	context 'on initialize' do
 
@@ -15,6 +19,32 @@ describe Dishes do
 			expect(ctb.name).to eq("Chicken Teriyaki Bento")
 		end 
 
+		# it 'a dish can have ingredients' do
+		# end
+	end
+
+	context 'different types of dishes' do
+
+		it 'has a price of 8.95 if it is Chicken Teryiyaki Bento' do
+			expect(ctb.price).to eq(8.95)
+		end
+
+		it 'has a price of 9.95 if it is Tonkotsu Ramen' do
+			expect(tr.price).to eq(9.95)
+		end
+
+		it 'has a price of 5.45 if it is Miso Soup' do
+			expect(ms.price).to eq(5.45)
+		end
+
+		it 'has a price of 12.00 if it is Sushi Selection' do
+			expect(ss.price).to eq(12.00)
+		end
+
+		it 'has a price of 15.00 if it is Wagyu Beef' do
+			expect(wb.price).to eq(15.00)
+		end
+	
 	end
 
 end
